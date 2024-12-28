2803f6cda371ca09e48055e55542f24865214412
Akshatf <akshatfarkya7@gmail.com>
2024-12-28 21:18:12 +0530

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import Projects from "./components/sections/Projects";
+import Experience from "./components/sections/Experience";
+import Education from "./components/sections/Education";
+// import StartCanvas from "./components/canvas/Stars";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
+import Chatbot from "./components/sections/Chatbot";
+// import Preloader from "./components/sections/Preloader";
+// import { Loader } from "@react-three/drei";
+import Loader from "./components/sections/Loader";
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+
+  const [isLoading , setIsLoading]=useState(true);
+  useEffect(()=>{
+    const fakeDataFetch = ( ) => {
+      setTimeout(() =>{
+        setIsLoading(false);
+      },4000);
+
+    }
+    fakeDataFetch();
+  },[])
+
+  return isLoading ? (
+    <Loader/>
+  ) : (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+            <>
+              {/* <StartCanvas /> */}
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Hero />
+                      <Contact />
+                      <Footer />
+                      <Chatbot/>
+                    </>
+                  }
+                />
+                {/* Render both Skills and Projects on the same route */}
+                <Route
+                  path="/skills"
+                  element={
+                    <>
+                      <Skills />
+                      <Education/>
+                      <Footer/>
+                      <Chatbot/>
+                    </>
+                  }
+                />
+                  <Route
+                  path="/projects"
+                  element={
+                    <>
+                      <Projects />
+                      <Experience />
+                      <Footer/>
+                      <Chatbot/>
+                    </>
+                  }
+                />
+                {/* <Route path="/projects" element={<Projects />} /> */}
+                {/* <Route path="/education" element={<Education />} /> */}
+                {/* <Route path="/chatbot" element={<Chatbot />} /> */}
+              </Routes>
+            </>
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/sections/Contact.jsx
@@ -0,0 +1,245 @@
+// sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+// mysitekey =6LdD2EwqAAAAAPlz_a8d_GtjGRIl5r4jkWEdYkip" site key
+// secretkey ="6LdD2EwqAAAAAKj84tR1_hTjvR1485voUbdMll-E"
+//web3 "2caa3035-88da-4425-92ce-b3131b5ed9ca"
+
+import React, { useState, useRef } from "react";
+import { useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";
+import styled from "styled-components";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(17, 25, 40, 0.83);
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  padding: 32px;
+  border-radius: 12px;
+  box-shadow: rgba(23, 92, 230, 0.1) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`;
+
+const ContactTitle = styled.div`
+  font-size: 28px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const ContactInput = styled.input`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const ContactInputMessage = styled.textarea`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const ContactButton = styled.input`
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  padding: 13px 16px;
+  margin-top: 2px;
+  border-radius: 12px;
+  border: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+const Message = styled.p`
+  font-size: 16px;
+  color: white; /* This sets the message color to white */
+  margin-top: 10px;
+`;
+const ErrorMessage = styled.p`
+  font-size: 14px;
+  color: white; /* Set the font color to white */
+  margin-top: 5px;
+`;
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({ mode: "onTouched" });
+
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+  const [verified, setVerified] = useState(false);
+
+  const apiKey = process.env.PUBLIC_ACCESS_KEY || "2caa3035-88da-4425-92ce-b3131b5ed9ca";
+
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: "Akshat's Portfolio",
+      subject: "New Responce",
+    },
+    onSuccess: (msg) => {
+      setIsSuccess(true);
+      setMessage(msg);
+      reset();
+    },
+    onError: (msg) => {
+      setIsSuccess(false);
+      setMessage(msg);
+    },
+  });
+
+  const handleCaptchaChange = (value) => {
+    setVerified(!!value);
+  };
+
+  return (
+    <Container id="Contact">
+      <Wrapper>
+        <Title>Contact</Title>
+        <Desc>Feel free to reach out to me for any questions , opportunities or collaborations !</Desc>
+        <Desc>You can Contact me via <a href="mailto:akshatfarkya07@gmail.com">Email</a> or fill the below form , I'll get in your touch </Desc>
+        <a href="https://wa.me/919425718644?text=Hey!%20I%20am%20Akshat.%20I%20would%20like%20to%20connect%20with%20you." target="_blank">Send a message on WhatsApp</a>
+        <ContactForm onSubmit={handleSubmit(onSubmit)}>
+          <ContactTitle>Email Me 🚀</ContactTitle>
+
+          <ContactInput
+            placeholder="Your Name"
+            {...register("name", {
+              required: "Full name is required",
+              maxLength: 80,
+            })}
+            className={errors.name ? "error" : ""}
+          />
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+
+          <ContactInput
+            placeholder="Your Email"
+            {...register("email", {
+              required: "Enter your email",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Please enter a valid email",
+              },
+            })}
+            className={errors.email ? "error" : ""}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+
+          <ContactInput
+            placeholder="Subject"
+            {...register("subject", {
+              required: "Enter a subject",
+            })}
+            className={errors.subject ? "error" : ""}
+          />
+          {errors.subject && <ErrorMessage>{errors.subject.message}</ErrorMessage>}
+
+          <ContactInputMessage
+            placeholder="Message"
+            rows={4}
+            {...register("message", {
+              required: "Enter your message",
+            })}
+            className={errors.message ? "error" : ""}
+          />
+          {errors.message && <ErrorMessage>{errors.message.message}</ErrorMessage>}
+
+          <ReCAPTCHA
+            sitekey="6LdD2EwqAAAAAPlz_a8d_GtjGRIl5r4jkWEdYkip"
+            onChange={handleCaptchaChange}
+          />
+
+          <ContactButton
+            type="submit"
+            value={isSubmitting ? "Sending..." : "Send Message"}
+            disabled={!verified || isSubmitting}
+          />
+        </ContactForm>
+
+        {isSubmitSuccessful && isSuccess && (
+          <Message>{message || "Message sent successfully!"}</Message>
+        )}
+        {isSubmitSuccessful && !isSuccess && (
+          <Message>{message || "Something went wrong!"}</Message>
+        )}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
+
