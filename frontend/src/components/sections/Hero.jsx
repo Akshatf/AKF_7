2d064943c6c538b462405293c2d064575f1cb614
Akshatf <akshatfarkya7@gmail.com>
2025-08-02 02:47:35 +0530

    UI Fixed

--- frontend/src/components/sections/Hero.jsx
+++ frontend/src/components/sections/Hero.jsx
@@ -163,13 +163,22 @@ const SubTitle = styled.div`
   }
 `;
 
+
 const ResumeButton = styled.a`
+  -webkit-appearance: button;
+  -moz-appearance: button;
+  appearance: button;
   text-decoration: none;
   width: 95%;
   max-width: 300px;
   text-align: center;
   padding: 16px 0;
-  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
   box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
   border-radius: 50px;
   font-weight: 600;
@@ -178,6 +187,7 @@ const ResumeButton = styled.a`
   &:hover {
     transform: scale(1.05);
     transition: all 0.4s ease-in-out;
+    box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
     filter: brightness(1);
   }
   @media (max-width: 640px) {
@@ -185,6 +195,7 @@ const ResumeButton = styled.a`
     font-size: 18px;
   }
 `;
+
 
 const Img = styled.img`
   width: 69%;
